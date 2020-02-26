1764321b0e7254b815444d561ccf67a0ac8a4f50
HanulYun <hanulyun@peppykorea.com>
2020-02-26 09:41:53 +0900

    Create Project File

---
+++ CoreDataDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreDataDemo
+//
+//  Created by HanulYun-Comp on 2020/02/26.
+//  Copyright © 2020 Yun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
